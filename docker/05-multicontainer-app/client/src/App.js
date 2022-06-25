b8e9713c4258d412035f4314029f657c925029d0
TanoyB Personal <tanoybhowmick@gmail.com>
2022-06-25 15:58:33 +0530

    react-app

---
+++ docker/05-multicontainer-app/client/src/App.js
@@ -0,0 +1,36 @@
+import logo from './logo.svg';
+import './App.css';
+
+import {BrowserRouter as Router, Route, Link } from 'react-router-dom';
+import Otherpage from './Otherpage';
+import Fib from './Fib';
+
+function App() {
+  return (
+    <Router>
+      <div className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          
+          <a
+            className="App-link"
+            href="https://reactjs.org"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Learn React
+          </a>
+          <p></p>
+          <Link to='/'>Home</Link>
+          <Link to='/otherpage'>Other Page</Link>
+        </header>
+        <div>
+            <Route exact path='/' component={Fib} />
+            <Route exact path='/otherpage' component={Otherpage} />
+          </div>
+      </div>
+    </Router>
+  );
+}
+
+export default App;
---
+++ docker/05-multicontainer-app/server/index.js
@@ -0,0 +1,66 @@
+const keys =require('./keys');
+
+//express app setup
+const express = require('express')
+const bodyParser = require('body-parser');
+const cors = require('cors');
+
+const app = express();
+app.use(cors());
+app.use(bodyParser.json());
+
+//postgres client setup
+const {pool} = require('pg');
+const pgClient = new pool({
+    user: keys.pgUser,
+    host: keys.pgHost,
+    port: keys.pgPort,
+    database: keys.pgDatabase,
+    password: keys.pgPassword
+});
+
+pgClient.on("connect", (client) => {
+    client
+      .query("CREATE TABLE IF NOT EXISTS values (number INT)")
+      .catch((err) => console.error(err));
+});
+
+//redis client setup
+const redis = require('redis')
+const redisClient = redis.createClient({
+    host: keys.redisHost,
+    port: keys.redisPort,
+    retry_strategy: () => 1000
+});
+const redisPublisher = redisClient.duplicate();
+
+//express route handlers
+app.get('/', (req, res) => {
+    res.send('Hi');
+});
+
+app.get('/values/all', async(req,res)=>{
+    const values = await pgClient.query('select * from values;');
+    res.send(values.rows);
+});
+app.get('/values/current', async(req,res)=>{
+
+    redisClient.hgetall('values', (err, values) =>{
+        res.send(values)
+    });
+});
+
+app.post('values', async (req, res) =>{
+    const index = req.body.index;
+    if (parseInt(index) > 20){
+        res.status(422).send('value is too high');
+    }
+    redisClient.hset('values', index, 'Nothing yet');
+    redisPublisher.publish('insert', index);
+    pgClient.query('INSERT INTO values (number) VALUES ($1)', [index]);
+    res.send({working: true});
+});
+
+app.listen(5000, err => {
+    console.log('listeninig')
+});
