37ae2dea32685501a12d6b3072cf3cb9842c47b1
TanoyB Personal <tanoybhowmick@gmail.com>
2023-05-06 01:34:47 +0530

    m

--- ds-algo/00-algo-practice/00-practice-board/main.go
+++ ds-algo/00-algo-practice/00-practice-board/main.go
@@ -1,3 +1,32 @@
 package main
 
-func main() {}
+import "fmt"
+
+func GenerateBrakets(s string, n, currentIndex, countOpen, countClose int) {
+	if currentIndex >= n*2 {
+		if len(s) > 0 {
+			Srune := []rune(s)
+			firstChar := string(Srune[0])
+			lastCar := string(Srune[len(s)-1])
+			if firstChar == ")" || lastCar == "(" {
+				return
+			}
+
+		}
+		fmt.Println(s)
+		return
+	}
+	//start with open
+	if countOpen < n {
+		GenerateBrakets(s+"(", n, currentIndex+1, countOpen+1, 0)
+	}
+	//with close
+	if countClose < countOpen {
+		GenerateBrakets(s+")", n, currentIndex+1, countOpen, countClose+1)
+	}
+
+}
+
+func main() {
+	GenerateBrakets("", 4, 0, 0, 0)
+}
--- ds-algo/14-dynamic-prog/02-dp-ud/02-backtracking/04-generate-brakets/main.go
+++ ds-algo/14-dynamic-prog/02-dp-ud/02-backtracking/04-generate-brakets/main.go
@@ -24,3 +24,30 @@ func GenerateBrakets(output string, n, open, close, currentIndex int) {
 func main() {
 	GenerateBrakets("", 3, 0, 0, 0)
 }
+
+/*
+func GenerateBrakets(s string, n, currentIndex, countOpen, countClose int) {
+	if currentIndex >= n*2 {
+		if len(s) > 0 {
+			Srune := []rune(s)
+			firstChar := string(Srune[0])
+			lastCar := string(Srune[len(s)-1])
+			if firstChar == ")" || lastCar == "(" {
+				return
+			}
+
+		}
+		fmt.Println(s)
+		return
+	}
+	//start with open
+	if countOpen < n {
+		GenerateBrakets(s+"(", n, currentIndex+1, countOpen+1, 0)
+	}
+	//with close
+	if countClose < countOpen {
+		GenerateBrakets(s+")", n, currentIndex+1, countOpen, countClose+1)
+	}
+
+}
+*/
